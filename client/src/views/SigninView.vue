aebec6d9e84180bc5ab022502d6966e01a8b6144
SethTobias <seth.adams1107@gmail.com>
2024-03-11 22:55:38 +0200

    Basic Styling Sign In/Out

--- client/src/views/SigninView.vue
+++ client/src/views/SigninView.vue
@@ -1,19 +1,89 @@
 <template>
-  <div id="signin">
-    <div class="signin-form">
-      <div class="signin-txt">
-        <p>Some Text</p>
-      </div>
-      <div class="signin-input">
-        <input type="text" />
-        <input type="text" />
-        <input type="submit" />
-      </div>
-      <div class="signin-up">No account? Consider signing up!</div>
+    <div id="signin">
+      <h1>Literary Connect</h1>
+      <img src="https://i.ibb.co/HChwBLC/Logo.png" alt="site Logo" />
+      <form class="signin-form">
+        <div class="signin-txt">
+          <p>Some Text</p>
+        </div>
+        <div class="signin-input">
+          <input type="text" />
+          <input type="text" />
+          <input type="submit" />
+        </div>
+        <div class="signin-up">No account? Consider signing up!</div>
+      </form>
     </div>
-  </div>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+:root {
+  --txt: hsl(180, 44%, 93%);
+  --bg: hsl(180, 40%, 2%);
+  --primary: hsl(180, 66%, 47%);
+  --secondary: hsl(223, 65%, 29%);
+  --accent: hsl(242, 93%, 38%);
+}
+
+* {
+  color: var(--txt);
+}
+
+#signin {
+  /*  */
+  height: 700px;
+  width: 500px;
+  /*  */
+  margin: auto;
+  border-radius: 25px;
+  /*  */
+  background-image: url("https://i.ibb.co/Bzh0B30/BG-1.jpg");
+  /*  */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  /*  */
+  text-align: center;
+}
+
+#signin :is(h1, img) {
+  justify-self: start;
+}
+#signin h1 {
+  font-size: 3rem;
+}
+#signin img {
+  /*  */
+  height: auto;
+  width: 200px;
+  /*  */
+  margin: 0 auto;
+}
+.signin-txt,
+.signin-up {
+  font-size: 2rem;
+}
+
+.signin-up {
+  justify-self: end;
+}
+
+:is(.sigin-form),
+:is(.signin-input) {
+  /*  */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-content: center;
+}
+
+.signin-input input {
+  /*  */
+  width: 250px;
+  /*  */
+  margin: 5px auto;
+}
+</style>
--- client/src/views/SignupView.vue
+++ client/src/views/SignupView.vue
@@ -1,31 +1,90 @@
 <template>
   <div id="signup">
-    <div class="signup-form">
-      <div class="signup-img">
-        <img src="" alt="" />
-        <h1>Unlock A World Of Literary Connections.</h1>
-      </div>
-      <div class="signup-input">
-        <div class="signup-logo">
-          <div class="logo-container">
-            <i class="fa-solid fa-book-open"></i>
-            <i class="fa-solid fa-link"></i>
-          </div>
-          <h1>Literary Connect</h1>
+      <div class="signup-form">
+        <div class="signup-img">
+          <h1>Unlock A World Of Literary Connections.</h1>
         </div>
-        <form action="">
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="submit" />
-        </form>
+        <div class="signup-input">
+          <div class="signup-logo">
+            <img src="https://i.ibb.co/HChwBLC/Logo.png" alt="Site Logo">
+            <h1>Literary Connect</h1>
+          </div>
+          <form action="">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="submit" />
+          </form>
+        </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+:root {
+    --txt: hsl(180, 44%, 93%);
+    --bg: hsl(180, 40%, 2%);
+    --primary: hsl(180, 66%, 47%);
+    --secondary: hsl(223, 65%, 29%);
+    --accent: hsl(242, 93%, 38%);
+  }
+
+  #signup {
+    /*  */
+    height: 800px;
+    width: 800px;
+    /*  */
+    margin: auto;
+    /*  */
+    border-radius: 25px;
+  }
+
+  .signup-form {
+    /*  */
+    display: grid;
+    grid-template-areas: "img input";
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 100%;
+  }
+
+  .signup-img {
+    grid-area: img;
+    /*  */
+    background-image: url('https://i.ibb.co/F6zPWj8/BG-5.jpg');
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    border-radius:25px 0 0 25px;
+
+    /*  */
+    text-align: center;
+  }
+
+  .signup-input {
+    grid-area: input;
+    border-radius:0 25px  25px  0;
+    background-color: var(--accent);
+  }
+  .signup-input img{
+    width: 200px;
+    margin: 0 auto;
+    /* grid-area: input;
+    border-radius:25px 0 0 25px;
+    background-color: var(--accent); */
+  }
+  .signup-logo{
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    /*  */
+    text-align: center;
+  }
+  .signup-input form{
+    display: flex;
+    flex-direction: column;
+  }
+</style>
