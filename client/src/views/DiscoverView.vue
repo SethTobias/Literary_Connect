be494bb2d189c102d8a067f67d27ee491e97e53b
SethTobias <seth.addams1107@gmail.com>
2024-03-13 15:09:21 +0200

    Discover Styling Additions

--- client/src/views/DiscoverView.vue
+++ client/src/views/DiscoverView.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="discover">
     <Sign v-if="notSign"/>
-    <h1>Catch up on what's new !</h1>
+    <h1>Catch up on what's new!</h1>
     <div class="discover-container">
-      <div class="discover-post">
+            <div class="discover-post">
+              <div class="post-content">
+
+          <p>I like you</p>
+              </div>
         <div class="post-info">
-          <p><span>Shares</span><span>Comments</span></p>
+          <p><span>Shares</span> <span>Comments</span></p>
         </div>
       </div>
+            <div class="discover-post">
+        <div class="post-info">
+          <p><span>Shares</span> <span>Comments</span></p>
+        </div>
+      </div>
+            <div class="discover-post">
+        <div class="post-info">
+          <p><span>Shares</span> <span>Comments</span></p>
+        </div>
+      </div>
+            <div class="discover-post">
+        <div class="post-info">
+          <p><span>Shares</span> <span>Comments</span></p>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -26,3 +46,42 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ .discover-container {
+  display: grid;
+  grid: auto-flow 200px / repeat(3,1fr);  
+  gap: 25px;
+  place-items: center;
+ }
+
+ .discover-container .discover-post{
+  display: grid;
+  grid: 1fr / 1fr;
+ }
+ .discover-container .discover-post .post-content{
+  grid-area: 1/2/1/2;
+  background-color: red;
+ }
+ .discover-container .discover-post .post-content:hover{
+  z-index: -2;
+  filter: grayscale(.9);
+  opacity: .6;
+   /* display: none; */
+ }
+ .discover-container .discover-post .post-info{
+   grid-area: 1/2/1/2;
+   z-index: -1;
+ }
+ .discover-container .post-info{
+ }
+ 
+ /* .discover-container .discover-post .post-info p span{
+  display: none;  
+  z-index: -2;
+ }
+ .discover-container .discover-post .post-info p span:hover {
+  z-index: 2;
+  color: aqua;
+ } */
+</style>
